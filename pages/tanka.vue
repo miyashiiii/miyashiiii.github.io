ca9959a648540c20b551e8dd1519bfddf0e66553
Miyashita Yosuke <yosuke.miyashita@gmail.com>
2024-06-16 10:31:04 +0900

    スタイル修正

--- pages/tanka.vue
+++ pages/tanka.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row justify-center">
-    <div style="width: 940px">
+    <div style="width: 500px">
       <div v-if="isValidQuery" class="q-mt-md q-ml-md">
         タグ:
         <q-chip
@@ -10,53 +10,33 @@
           @remove="() => $router.push('/tanka')"
         />
       </div>
-      <!-- PC向け -->
-      <div class="q-pa-lg row items-start q-gutter-lg gt-sm">
-        <q-card
-          v-for="tanka in tankas"
-          :href="tanka.postUrl"
-          style="width: 280px"
-        >
-          <q-card-section>
-            <a :href="tanka.postUrl" class="text-bold text-black no-decoration">
-              {{ tanka.tanka }}
-            </a>
-            <div class="q-mt-xs text-grey-8">
-              <span class="text-black">{{ tanka.postDate }}</span> -
-              {{ tanka.postTitle }}
-            </div>
-            <NuxtLink :to="`/tanka?tag=${tag}`" v-for="tag in tanka.tags">
-              <q-chip class="text-caption q-ml-none" :label="tag" />
-            </NuxtLink>
-          </q-card-section>
-        </q-card>
-      </div>
-    </div>
 
-    <!-- スマホ向け -->
-    <div class="q-mt-sm lt-md">
-      <q-list separator>
-        <q-item
-          class="q-gutter-x-md items-center bg-white"
-          v-for="tanka in tankas"
-        >
-          <div class="col">
-            <a :href="tanka.postUrl" class="no-decoration">
-              <div class="text-black text-bold">
-                {{ tanka.tanka }}
-              </div>
-              <div class="q-mt-xs text-grey-8">
-                <span class="text-black">{{ tanka.postDate }}</span> -
-                {{ tanka.postTitle }}
-              </div>
-            </a>
-            <NuxtLink :to="`/tanka?tag=${tag}`" v-for="tag in tanka.tags">
-              <q-chip class="text-caption q-ml-none" :label="tag" />
-            </NuxtLink>
-          </div>
-        </q-item>
-        <q-separator color="grey-4" />
-      </q-list>
+      <div class="q-mt-sm">
+        <q-list separator>
+          <q-item
+            class="q-gutter-x-md items-center bg-white"
+            v-for="tanka in tankas"
+          >
+            <div class="col">
+              <a :href="tanka.postUrl" class="no-decoration">
+                <div class="text-black text-bold">
+                  {{ tanka.tanka }}
+                </div>
+                <div class="q-mt-xs text-grey-8 text-right">
+                  <span class="text-black">{{ tanka.postDate }}</span> -
+                  {{ tanka.postTitle }}
+                </div>
+                <div class="row justify-end">
+                  <NuxtLink :to="`/tanka?tag=${tag}`" v-for="tag in tanka.tags">
+                    <q-chip class="text-caption q-ml-none" :label="tag" />
+                  </NuxtLink>
+                </div>
+              </a>
+            </div>
+          </q-item>
+          <q-separator color="grey-4" />
+        </q-list>
+      </div>
     </div>
   </div>
 </template>
@@ -78,11 +58,9 @@ type Post = {
   date: string;
   tankas: Tanka[];
 };
-import { is } from "quasar";
 // TODO 一旦無視。後でちゃんと対応したい #6
 // @ts-ignore
 import tankaPostsJson from "./assets/tankaPosts.json";
-const posts: Ref<Post[]> = ref([]);
 const tankas: Ref<Tanka[]> = ref([]);
 const isValidQuery = ref(false);
 const route = useRoute();
@@ -115,29 +93,4 @@ watchEffect(() => {
     isValidQuery.value = true;
   }
 });
-
-type Service = {
-  name: string;
-  icon: string;
-};
-const getPostNameFromUrl = (url: string): Service => {
-  if (url.includes("hatenablog")) {
-    return { name: "はてなブログ", icon: "/sns/hatenablog.svg" };
-  } else if (url.includes("qiita")) {
-    return { name: "Qiita", icon: "/sns/qiita.png" };
-  } else if (url.includes("note")) {
-    return { name: "note", icon: "/sns/note.png" };
-  } else if (url.includes("zenn")) {
-    return { name: "Zenn", icon: "/sns/zenn.png" };
-  } else if (url.includes("docswell")) {
-    return { name: "Docswell", icon: "/sns/docswell.png" };
-  } else if (url.includes("sizu.me")) {
-    return { name: "しずかなインターネット", icon: "/sns/sizu.png" };
-  } else {
-    throw new Error("invalid url");
-  }
-};
-const getServiceNameFromUrl = (url: string): string => {
-  return getPostNameFromUrl(url).name;
-};
 </script>
